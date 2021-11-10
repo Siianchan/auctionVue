fa5fc970b1005c6c5f6df62da2c67f8e49c2718a
79479334 <79479334@qq.com>
2021-11-10 14:11:03 +0800

    main

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,28 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png" />
+    <TopMenu />
+    <!-- <img src="./assets/logo.png" /> -->
     <router-view />
   </div>
 </template>
 
 <script>
+import TopMenu from "@/components/TopMenu";
 export default {
   name: "App",
+  components: {
+    TopMenu,
+  },
 };
 </script>
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
+  padding: 0;
+}
+body {
+  margin: 0;
+  padding: 0;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+import HelloWorld from '@/components/Index'
+import LoginPage from '@/components/Login'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
+      name: 'Index',
       component: HelloWorld
+    },
+    {
+      path: '/login',
+      name: 'LoginPage',
+      component: LoginPage
     }
   ]
 })
